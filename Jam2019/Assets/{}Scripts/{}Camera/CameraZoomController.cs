01c69e3ea0071694137ec9a1fb5c9e3022976a44
Rorazoro <rorazoro@gmail.com>
2019-08-26 21:11:07 -0400

    Switched player models

---
+++ Jam2019/Assets/{}Scripts/{}Camera/CameraZoomController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CameraZoomController : MonoBehaviour
+{
+    private CinemachineVirtualCamera vcam;
+
+    public float minFov = 15f;
+    public float maxFov = 90f;
+    public float sensitivity = 10f;
+
+    void Start()
+    {
+        vcam = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fov = vcam.m_Lens.FieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        vcam.m_Lens.FieldOfView = fov;
+    }
+}
